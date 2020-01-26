57391779c53e37e619d26556f91d2fb617297391
David Ledford <dledford16@icloud.com>
2020-01-26 16:46:26 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fireball
+
+This is only to get me back up to date with using rails and github.. There are no expectations of what I want to do with this at this point. Just messing around and trying to learn all that I once knew, as I feel like this is all new to me again.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fireball::Application.config.secret_token = 'cca37e72e40037c8a559f573a2ae5cdd7efe2cf8a983ad463d3b04e07bd5a3dc0b18981111e0b02e99f7f27acdff655faa54a7baa88c91aef66abc0032a2bf59'
